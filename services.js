6231f59ca5ba0fd03983768dd3ec63192ce2ad3e
DrEconomist <kgalalelomonchusi09@gmail.com>
2026-04-29 21:19:12 +0200

    Create services.js

---
+++ services.js
@@ -0,0 +1,28 @@
+function Services() {
+  return (
+    <div className="container">
+      <h1>Products & Services</h1>
+
+      <div className="grid">
+        <div className="card">
+          <h3>Translation Pen</h3>
+          <p>Speech + text translation</p>
+        </div>
+
+        <div className="card">
+          <h3>Pricing</h3>
+          <p>R1499 Standard</p>
+          <p>R999 Student</p>
+          <p>R150/week Rental</p>
+        </div>
+
+        <div className="card">
+          <h3>Target Users</h3>
+          <p>Students, lecturers, travelers</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
